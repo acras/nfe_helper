16d841799a672eb80b036ce6de5213f23b394ba1
Romulo Ceccon <romulo@acras.com.br>
2010-11-24 17:09:28 +0000

    - removida a responsabilidade do client de inicializar o trust store (agora o próprio server o inicializa através da linha de comando pelas variáveis javax.net.ssl.trustStore e javax.net.ssl.trustStorePassword)

--- helper_server.cmd
+++ helper_server.cmd
@@ -1,1 +1,1 @@
-@java -cp %~dp0dist\acras.jar br.com.acras.nfe.HelperServer %*
+@java -cp %~dp0dist\acras.jar -Djavax.net.ssl.trustStore=lib\certificates\trusted.jks -Djavax.net.ssl.trustStorePassword=acrasnfe br.com.acras.nfe.HelperServer %*
--- src/br/com/acras/nfe/WebServiceClientInitializationHandler.java
+++ src/br/com/acras/nfe/WebServiceClientInitializationHandler.java
@@ -1,14 +1,5 @@
 package br.com.acras.nfe;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.File;
-
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-
 import java.util.Map;
 
 import javax.net.ssl.HostnameVerifier;
@@ -16,11 +7,6 @@ import javax.net.ssl.SSLSession;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-
-import br.com.acras.utils.GenericEncryption;
-import br.com.acras.utils.GenericEncryptionException;
 
 class WebServiceClientInitializationHandler extends CustomHttpHandler
 {
@@ -39,16 +25,13 @@ class WebServiceClientInitializationHandler extends CustomHttpHandler
   protected void handle(CustomHttpExchange exchange) throws Exception
   {
     String keyStoreId = exchange.getParameter("keystoreid");
-    String trustStoreFile = baseDirectory + File.separator +
-        exchange.getParameter("truststorefile");
-    String trustStorePassword = exchange.getParameter("truststorepassword");
 
     KeyEntryReference keRef = keyEntryMap.get(keyStoreId);
     if (keRef == null)
       throw new NotFoundException("No key entry with id " + keyStoreId +
           " was found (not initialized?)");
 
-    initSSLSocketFactory(keRef, trustStoreFile, trustStorePassword);      
+    initSSLSocketFactory(keRef);
   }
   
   protected String getAllowedMethod()
@@ -56,13 +39,12 @@ class WebServiceClientInitializationHandler extends CustomHttpHandler
     return "GET";
   }
   
-  private synchronized void initSSLSocketFactory(KeyEntryReference keRef,
-      String trustStoreFile, String trustStorePassword) throws Exception
+  private synchronized void initSSLSocketFactory(KeyEntryReference keRef) throws Exception
   {
     SSLContext context = SSLContext.getInstance("SSL");
     context.init(
         createKeyManagers(keRef),
-        createTrustManagers(trustStoreFile, trustStorePassword),
+        null,
         null);
     
     HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
@@ -78,49 +60,6 @@ class WebServiceClientInitializationHandler extends CustomHttpHandler
             }  
           });
     }
-  }
-  
-  private TrustManager[] createTrustManagers(String trustStoreFile, String trustStorePassword)
-      throws Exception
-  {
-    FileInputStream inputStream;
-    String password;
-    
-    try
-    {
-      inputStream = new FileInputStream(trustStoreFile);
-    }
-    catch(FileNotFoundException e)
-    {
-      throw new NotFoundException("Trust store " + trustStoreFile + " does not exist");
-    }
-    
-    try
-    {
-      password = GenericEncryption.decryptString(trustStorePassword);
-    }
-    catch(GenericEncryptionException e)
-    {
-      throw new BadRequestException("Trust store password not properly encoded");
-    }
-    
-    KeyStore ks = KeyStore.getInstance("JKS");
-    
-    try
-    {
-      ks.load(inputStream, password.toCharArray());
-    }
-    catch(Exception e)
-    {
-      throw new ForbiddenException(e);
-    }
-    
-    TrustManagerFactory tmFactory;
-    
-    tmFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-    tmFactory.init(ks);
-    
-    return tmFactory.getTrustManagers();
   }
   
   private KeyManager[] createKeyManagers(KeyEntryReference keRef)
